4e7347c691357effc3a3ead1923da0fab394b5d8
khaled <khaledb.yahya@gmail.com>
2023-03-11 18:15:11 +0100

    added movieInfo & actors components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { CssBaseline } from "@mui/material";
 import { Routes, Route } from "react-router-dom";
-import { Movies, Navbar } from "./index";
+import { Navbar, Movies, MovieInfo, Actors } from "./index";
 import useStyles from "./styles";
 import ToggleTheme from "../utils/ToggleTheme";
 
@@ -17,9 +17,9 @@ function App() {
           <Routes>
             <Route exact path="/" element={<Movies />} />
             <Route exact path="/approved" element={<Movies />} />
-            {/* <Route exact path="/movie/:id" element={<MovieInfo />} />
-              <Route exact path="/actors/:id" element={<Actors />} />
-              <Route exact path="/profile/:id" element={<Profile />} /> */}
+            <Route exact path="/movie/:id" element={<MovieInfo />} />
+            <Route exact path="/actors/:id" element={<Actors />} />
+            {/* <Route exact path="/profile/:id" element={<Profile />} /> */}
           </Routes>
         </main>
       </div>
--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,10 @@
-export { default as Movies } from "./Movies/Movies";
 export { default as Navbar } from "./Navbar/Navbar";
-export { default as Sidebar } from "./Sidebar/Sidebar";
 export { default as Search } from "./Search/Search";
+export { default as Sidebar } from "./Sidebar/Sidebar";
+export { default as FeaturedMovie } from "./FeaturedMovie/FeaturedMovie";
+export { default as Movies } from "./Movies/Movies";
 export { default as MovieList } from "./MovieList/MovieList";
 export { default as Movie } from "./Movie/Movie";
+export { default as MovieInfo } from "./MovieInfo/MovieInfo";
+export { default as Actors } from "./Actors/Actors";
 export { default as Pagination } from "./Pagination/Pagination";
-export { default as FeaturedMovie } from "./FeaturedMovie/FeaturedMovie";
