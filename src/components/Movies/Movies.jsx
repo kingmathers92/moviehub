fe2ab9c22b49c86669ec8b5b620f009572c12d0f
khaled <khaledb.yahya@gmail.com>
2023-03-18 13:09:01 +0100

    popular movies as initialState

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -16,7 +16,7 @@ function Movies() {
     (state) => state.genreOrCategory
   );
   const { data, error, isFetching } = useGetMoviesQuery({
-    genreIdOrCategoryName,
+    genreIdOrCategoryName: "popular",
     page,
     searchQuery,
   });
