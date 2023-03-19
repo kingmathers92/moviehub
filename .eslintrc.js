7f4a4dc5cb55e8ca9ebf89841bc7a7e14d9a3cc7
khaled <khaledb.yahya@gmail.com>
2023-03-19 17:20:27 +0100

    fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,26 +5,37 @@ module.exports = {
   },
   extends: ["plugin:react/recommended", "airbnb"],
   parserOptions: {
+    parser: "babel-eslint",
+    requireConfigFile: false,
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: "latest",
     sourceType: "module",
   },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
   plugins: ["react"],
   rules: {
-    quotes: [2, "double"],
-    "import/extensions": 0,
+    "spaced-comment": [
+      "error",
+      "never",
+      {
+        block: {
+          balanced: true,
+        },
+      },
+    ],
+    "import/no-cycle": 0,
+    "no-console": 0,
     "react/prop-types": 0,
     "linebreak-style": 0,
     "react/state-in-constructor": 0,
     "import/prefer-default-export": 0,
     "max-len": [2, 250],
-    "no-multiple-empty-lines": ["error", { max: 1, maxEOF: 1 }],
-    "no-underscore-dangle": [
-      "error",
-      { allow: ["_d", "_dh", "_h", "_id", "_m", "_n", "_t", "_text"] },
-    ],
     "object-curly-newline": 0,
     "react/jsx-filename-extension": 0,
     "react/jsx-one-expression-per-line": 0,
@@ -35,13 +46,9 @@ module.exports = {
     "react/no-array-index-key": 0,
     "no-param-reassign": 0,
     "react/react-in-jsx-scope": 0,
-    "jsx-a11y/anchor-is-valid": [
-      "error",
-      {
-        components: ["Link"],
-        specialLink: ["to", "hrefLeft", "hrefRight"],
-        aspects: ["noHref", "invalidHref", "preferButton"],
-      },
-    ],
+    "react/jsx-props-no-spreading": 0,
+    "no-sparse-arrays": 0,
+    "no-array-index-key": 0,
+    camelcase: "allow",
   },
 };
--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,25 +1,47 @@
-import React from 'react';
-import { Typography, Button } from '@mui/material';
+import React from "react";
+import { Typography, Button } from "@mui/material";
 
-import useStyles from './styles';
+import useStyles from "./styles";
 
 function Pagination({ currentPage, setPage, totalPages }) {
   const classes = useStyles();
 
   const handlePrev = () => {
-    if (currentPage !== 1) { setPage((prevPage) => prevPage - 1); }
+    if (currentPage !== 1) {
+      setPage((prevPage) => prevPage - 1);
+    }
   };
   const handleNext = () => {
-    if (currentPage !== totalPages) { setPage((prevPage) => prevPage + 1); }
+    if (currentPage !== totalPages) {
+      setPage((prevPage) => prevPage + 1);
+    }
   };
 
   if (totalPages === 0) return null;
 
   return (
     <div className={classes.container}>
-      <Button onClick={handlePrev} variant="contained" className={classes.button} color="primary" type="button">Prev</Button>
-      <Typography variant="h4" className={classes.pageNumber}>{currentPage}</Typography>
-      <Button onClick={handleNext} variant="contained" className={classes.button} color="primary" type="button">Next</Button>
+      <Button
+        onClick={handlePrev}
+        variant="contained"
+        className={classes.button}
+        color="primary"
+        type="button"
+      >
+        Prev
+      </Button>
+      <Typography variant="h4" className={classes.pageNumber}>
+        {currentPage}
+      </Typography>
+      <Button
+        onClick={handleNext}
+        variant="contained"
+        className={classes.button}
+        color="primary"
+        type="button"
+      >
+        Next
+      </Button>
     </div>
   );
 }
